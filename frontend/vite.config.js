bd852424b03d8dd0399b84bb70f2c53abab8455c
Crispttr <cristian.pottier@heig-vd.ch>
2025-06-12 16:26:09 +0200

    vsjs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: '../backend/public',
+    emptyOutDir: false,
+  },
   server: {
     proxy: {
       '/api': {
